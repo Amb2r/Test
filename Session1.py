00e5f319bc011105d81683ad320270aded61b47b
Amb2r <amirbahadorch@gmail.com>
2024-09-25 13:45:51 +0330

    Add files via upload

---
+++ Session1.py
@@ -0,0 +1,6 @@
+name = "Amir Bahaor"
+lastname = "Cherghi"
+age = 35
+print ("My Name is " , name )
+print ("My lastName is " , lastname )
+print ("My Age is " , age )
---
+++ Tam1.py
@@ -0,0 +1,4 @@
+x = 20
+y = 15
+print ( "SUM = " , x + y )
+print ("AVG = " , (x+y)/2 )
